44e5c41d77a87534399d6a5ca72087d078a22c4a
Burhanos2 <24742@ma-web.nl>
2018-06-21 10:02:03 +0200

    PLACEHOLDERS GONE HALLALUJAH

--- Assets/Scripts/small scripts/appear.cs
+++ Assets/Scripts/small scripts/appear.cs
@@ -6,7 +6,7 @@ public class appear : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        gameObject.GetComponent<Renderer>().enabled = false;
+        gameObject.GetComponent<Renderer>().enabled = true;
     }
 	
     
@@ -14,7 +14,7 @@ public class appear : MonoBehaviour {
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Space))
         {
-            transform.position = new Vector3(0.1f, 0, 0);
+            transform.position = new Vector3(0.46f, -0.09f, 0);
             gameObject.GetComponent<Renderer>().enabled = true;
         }
 	}
